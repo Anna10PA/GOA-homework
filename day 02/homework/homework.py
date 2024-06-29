c605b948643566ba0cf72acfc3602c355317c714
Anna10PA <puturidzeana0210@gmail.com>
2024-06-29 10:46:57 +0400

    second commit

---
+++ day 02/homework/homework.py
@@ -0,0 +1,7 @@
+from turtle import*
+
+friend1=("Salome Mosesiashvili ")
+friend2=("Elene Garibovi ")
+friend3=("Nika Tavarkiladze")
+
+print("In Goal Academy I met " + friend1 + friend2 + friend3) 
