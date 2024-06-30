1d0087cf4c2c0636e4283ed017c798c9ff853309
Anna10PA <puturidzeana0210@gmail.com>
2024-06-30 11:57:55 +0400

    homework.py

--- day 03/homework/homework.py
+++ day 03/homework/homework.py
@@ -9,9 +9,3 @@ sport="shooting"
 
 print("I am "+ name + surname + str(age) + " years old, from " + city + ". My favorite countrys are " + Countrys + ". I am learning " + sport + ". And I like " + car + "and " + motorcycle + ". ")
 
-
-
-# print = input("Hello, what is your name?")
-# print = input("when did you born?")
-# age = input(int("Born year"))
-# print = (2024 - int("Born year"))
