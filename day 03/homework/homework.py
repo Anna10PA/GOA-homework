daa2be17950cf31cdc4098618386d4e65a687d8a
Anna10PA <puturidzeana0210@gmail.com>
2024-06-30 11:49:56 +0400

    commit

---
+++ day 03/homework/homework.py
@@ -0,0 +1,17 @@
+name = "Ana "
+surname = "Puturidze "
+car = "BMW "
+motorcycle = "kawasaki "
+age = 15
+city = "Tbilisi"
+Countrys = "Korea and USA"
+sport="shooting"
+
+print("I am "+ name + surname + str(age) + " years old, from " + city + ". My favorite countrys are " + Countrys + ". I am learning " + sport + ". And I like " + car + "and " + motorcycle + ". ")
+
+
+
+# print = input("Hello, what is your name?")
+# print = input("when did you born?")
+# age = input(int("Born year"))
+# print = (2024 - int("Born year"))
